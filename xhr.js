9523149019ef912a68f3746d3be51e91a8374046
yarov475 <59488110+yarov475@users.noreply.github.com>
2021-04-15 22:36:27 +0300

    promise

--- xhr.js
+++ xhr.js
@@ -1,17 +1,24 @@
-const req = 'https://jsonplaceholder.typicode.com/users';
-const xhr = new XMLHttpRequest();
-xhr.open('GET', req);
-xhr.responseType = 'json'
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+function sendRequest(method, url){
+        return new Promise( (resolve, reject)=> {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', requestURL);
+        xhr.responseType = 'json'
 
-xhr.onload = () => {
-    //return string
-    //console.log(JSON.parse(xhr.response ))
-    if (xhr.status >=400){
-        console.error(xhr.response)
-    } else
-    console.log(xhr.response)
+        xhr.onload = () => {
+            //return string
+            //console.log(JSON.parse(xhr.response ))
+            if (xhr.status >= 400) {
+                reject(xhr.response)
+            } else
+                resolve(xhr.response)
+        }
+        xhr.onerror = () => {
+            reject(xhr.response)
+        }
+        xhr.send();
+    })
 }
-xhr.onerror=()=>{
-    console.log(xhr.response)
-}
-xhr.send();
+sendRequest('GET', requestURL)
+    .then(data =>console.log(data))
+    .catch(err=>console.log(err))
